39f01842f898a860f1e10ae3aee979e3c88ef8e9
phillipsk <inquiries@kevinphillips.email>
2021-01-21 22:31:25 -0500

    research navUI, navController destination listeners

--- app/src/main/java/com/missionchurchcooljc/mcc/MainActivity.kt
+++ app/src/main/java/com/missionchurchcooljc/mcc/MainActivity.kt
@@ -3,10 +3,15 @@ package com.missionchurchcooljc.mcc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,6 +31,19 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             setupBottomNavigationBar()
         }
+
+//        TODO: to properly implement an icon on the appbar / tab icon / actionbar
+//          review these links, as of now the toolbar is under the Fragment navigation
+//          component label, creating a duplicate toolbar view, review these links and
+//          further to handle hiding the Androidx Nav component fragment label upon
+//          switching fragments with AndroixX navController and directions
+        val layout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        layout.setupWithNavController(toolbar, navController)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
@@ -37,21 +55,21 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.home_bottom_navigation)
 
         val navGraphIds = listOf(
-            R.navigation.aboutus,
-            R.navigation.connect,
-            R.navigation.listen,
-            R.navigation.pray,
-            R.navigation.info
+                R.navigation.aboutus,
+                R.navigation.connect,
+                R.navigation.listen,
+                R.navigation.pray,
+                R.navigation.info
         )
 //        val navGraphIds = listOf(R.navigation.nav_graph_main)
 //        val navGraphIds = listOf(R.navigation.nav_graph_main, R.navigation.list, R.navigation.form)
 
         // Setup the bottom navigation view with a list of navigation graphs
         val controller = bottomNavigationView.setupWithNavController(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.mainNavHostFragment,
-            intent = intent
+                navGraphIds = navGraphIds,
+                fragmentManager = supportFragmentManager,
+                containerId = R.id.mainNavHostFragment,
+                intent = intent
         )
 
         // Whenever the selected controller changes, setup the action bar.
