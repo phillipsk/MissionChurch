672b34871fd01ffc87ac4eafef27302ae36aae33
phillipsk <inquiries@kevinphillips.email>
2021-05-16 19:02:00 -0400

    GitHub Actions test #3

--- subtree-FMC/app/src/main/java/io/fmc/ui/videoplayer/PlayerActivity.java
+++ subtree-FMC/app/src/main/java/io/fmc/ui/videoplayer/PlayerActivity.java
@@ -1,23 +1,35 @@
+/*
+ * Copyright (c) 2021 Kevin Phillips, Mission Church of Our Lord Jesus Christ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.fmc.ui.videoplayer;
 
-import android.net.Uri;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.afollestad.easyvideoplayer.EasyVideoCallback;
-import com.afollestad.easyvideoplayer.EasyVideoPlayer;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.fmc.R;
-import io.fmc.R2;
 
-public class PlayerActivity extends AppCompatActivity implements EasyVideoCallback {
+public class PlayerActivity extends AppCompatActivity { //implements EasyVideoCallback {
 
-    @BindView(R2.id.player) EasyVideoPlayer player;
+//    @BindView(R2.id.player) EasyVideoPlayer player;
 
 
     String videoUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +39,11 @@ public class PlayerActivity extends AppCompatActivity implements EasyVideoCallba
 
         videoUrl = getIntent().getExtras().getString("video_url");
 
-        player.setCallback(this);
+//        player.setCallback(this);
 
         // Sets the source to the HTTP URL held in the TEST_URL variable.
         // To play files, you can use Uri.fromFile(new File("..."))
-        player.setSource(Uri.parse(videoUrl));
+//        player.setSource(Uri.parse(videoUrl));
     }
 
 
@@ -39,52 +51,52 @@ public class PlayerActivity extends AppCompatActivity implements EasyVideoCallba
     protected void onPause() {
         super.onPause();
 
-        player.pause();
+//        player.pause();
 
     }
 
-    @Override
-    public void onStarted(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onPaused(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onPreparing(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onPrepared(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onBuffering(int percent) {
-
-    }
-
-    @Override
-    public void onError(EasyVideoPlayer player, Exception e) {
-
-    }
-
-    @Override
-    public void onCompletion(EasyVideoPlayer player) {
-
-    }
-
-    @Override
-    public void onRetry(EasyVideoPlayer player, Uri source) {
-
-    }
-
-    @Override
-    public void onSubmit(EasyVideoPlayer player, Uri source) {
-
-    }
+//    @Override
+//    public void onStarted(EasyVideoPlayer player) {
+//
+//    }
+//
+//    @Override
+//    public void onPaused(EasyVideoPlayer player) {
+//
+//    }
+//
+//    @Override
+//    public void onPreparing(EasyVideoPlayer player) {
+//
+//    }
+//
+//    @Override
+//    public void onPrepared(EasyVideoPlayer player) {
+//
+//    }
+//
+//    @Override
+//    public void onBuffering(int percent) {
+//
+//    }
+//
+//    @Override
+//    public void onError(EasyVideoPlayer player, Exception e) {
+//
+//    }
+//
+//    @Override
+//    public void onCompletion(EasyVideoPlayer player) {
+//
+//    }
+//
+//    @Override
+//    public void onRetry(EasyVideoPlayer player, Uri source) {
+//
+//    }
+//
+//    @Override
+//    public void onSubmit(EasyVideoPlayer player, Uri source) {
+//
+//    }
 }
